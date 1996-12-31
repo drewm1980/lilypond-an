fb258ed2546fae89c94a7a088d0cd3b351182e1a
fred <fred>
1996-12-31 23:00:00 +0000

    lilypond-0.0.64

---
+++ lib/include/moment.hh
@@ -0,0 +1,15 @@
+/*
+  (c) 1996,97 Han-Wen Nienhuys
+*/
+
+#ifndef MOMENT_HH
+#define MOMENT_HH
+
+#include "rational.hh"
+class String;
+typedef Rational Moment;
+void print(Moment);
+
+
+#endif // 
+
