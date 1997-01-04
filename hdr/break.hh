afa5a927945b9ef6a784e722be5d1877f160343f
fred <fred>
1997-01-04 19:22:57 +0000

    lilypond-0.0.21

--- hdr/break.hh
+++ hdr/break.hh
@@ -1,43 +1,53 @@
 /*
   break.hh -- part of LilyPond
 
-  (c) 1996,97 Han-Wen Nienhuys
+  (c) 1996 Han-Wen Nienhuys
 */
 
 #ifndef BREAK_HH
 #define BREAK_HH
-#include "varray.hh"
+#include "vray.hh"
 #include "proto.hh"
-#include "colhpos.hh"
+typedef svec<PCol*>  Line_of_cols;
 
-/** Class representation of an algorithm which decides where to put
-  the column, and where to break lines.  */
+struct Col_configuration {
+    Line_of_cols cols;
+    svec<Real> config;
+    Real energy;
+
+    /****************/
+    void OK()const;
+    void setsol(svec<Real>);
+    Col_configuration() ;
+    void add( PCol*c);
+    void print() const;
+};
+
 struct Break_algorithm {
     PScore &pscore_;
     Real linelength;
 
-    /* *************** */
+    /****************/
 
     Break_algorithm(PScore&);
-
     /// check if the spacing/breaking problem is well-stated
     void problem_OK()const;
-
     /// search all pcols which are breakable.
-    Line_of_cols find_breaks() const;
+    svec<PCol *> find_breaks() const;
 
      /// helper: solve for the columns in #curline#.
-    Col_hpositions solve_line(Line_of_cols) const;
+    svec<Real> solve_line(Line_of_cols) const;
 
+    
     /// does curline fit on the paper?    
     bool feasible(Line_of_cols)const;
     
-    virtual Array<Col_hpositions> solve()=0;
+    virtual svec<Col_configuration> solve()=0;
 };
 
 /// wordwrap type algorithm: move to next line if current is optimal.
 struct Word_wrap : Break_algorithm {
-    virtual Array<Col_hpositions> solve();
+    virtual svec<Col_configuration> solve();
     Word_wrap(PScore&);
 };
 #endif // BREAK_HH
