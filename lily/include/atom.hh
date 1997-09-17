742910640fb6caa10f6fbcd0e295cc8a3ecfbeab
fred <fred>
1997-09-17 10:11:16 +0000

    lilypond-0.1.15

--- lily/include/atom.hh
+++ lily/include/atom.hh
@@ -1,17 +1,37 @@
 /*
-  atom.hh -- declare 
+  atom.hh -- declare Atom
 
   source file of the GNU LilyPond music typesetter
 
   (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
 */
 
-
 #ifndef ATOM_HH
 #define ATOM_HH
 
-#include "symbol.hh"
-#error
+#include "string.hh"
+#include "boxes.hh"
+#include "lily-proto.hh"
 
 
-#endif // ATOM_HH
+/// a symbol which can be translated, and freely copied
+struct Atom {
+  String tex_;
+  Box dim_;
+  Offset off_;
+
+  String str() const;		// for printing.
+  Atom (String, Box);
+    Atom ();
+  void translate (Offset o) {
+    off_ += o;
+  }
+  void translate (Real r,Axis a){
+    off_[a] += r;
+  }
+  /// how big is #this#?
+  Box extent() const;
+  void print() const;
+  String TeX_string() const;
+};
+#endif
