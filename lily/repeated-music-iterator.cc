ddb4fc751df55689a7942db4b71a692b45c5d64a
fred <fred>
1999-06-25 08:47:53 +0000

    lilypond-1.1.50

--- lily/repeated-music-iterator.cc
+++ lily/repeated-music-iterator.cc
@@ -1,136 +1,118 @@
 /*   
-  repeated-music-iterator.cc --  implement Repeated_music_iterator
+  new-repeated-music-iterator.cc --  implement Folded_repeat_iterator
   
   source file of the GNU LilyPond music typesetter
   
-  (c) 1998--1999 Jan Nieuwenhuizen <janneke@gnu.org>
+  (c) 1999 Han-Wen Nienhuys <hanwen@cs.uu.nl>
   
  */
 
-#include "repeated-music-iterator.hh"
-#include "repeated-music.hh"
-#include "musical-request.hh"
+#include "folded-repeat-iterator.hh"
+#include "new-repeated-music.hh"
+#include "music-list.hh"
+#include "simultaneous-music-iterator.hh"
 #include "translator-group.hh"
-#include "command-request.hh"
 
-Repeated_music_iterator::Repeated_music_iterator ()
+Folded_repeat_iterator::Folded_repeat_iterator ()
 {
-  repeat_iter_p_ = 0;
+  main_iter_p_ = 0;
   alternative_iter_p_ = 0;
-  here_mom_ = 0;
-  unfold_i_ = -1; 
 }
 
-Repeated_music_iterator::~Repeated_music_iterator ()
+bool
+Folded_repeat_iterator::ok () const
 {
-  delete repeat_iter_p_;
+  return main_iter_p_ || alternative_iter_p_;
+}
+
+Folded_repeat_iterator::~Folded_repeat_iterator ()
+{
+  delete main_iter_p_;
   delete alternative_iter_p_;
 }
 
-void
-Repeated_music_iterator::do_print () const
+Moment
+Folded_repeat_iterator::next_moment () const
 {
-  if (repeat_iter_p_) repeat_iter_p_->print ();
-  if (alternative_iter_p_) alternative_iter_p_->print ();
-}
-
-void
-Repeated_music_iterator::construct_children ()
-{
-  repeat_iter_p_ = get_iterator_p (dynamic_cast<Repeated_music const*> (music_l_)->repeat_p_);
-}
-
-void
-Repeated_music_iterator::do_process_and_next (Moment m)
-{
-  if (first_b_)
+  if (main_iter_p_)
     {
-      bool success = report_to_l ()->try_music (dynamic_cast<Repeated_music const*> (music_l_));
+      return main_iter_p_->next_moment ();
+    }
+  else
+    return main_length_mom_ + alternative_iter_p_->next_moment ();
+}
+
+void
+Folded_repeat_iterator::construct_children ()
+{
+  New_repeated_music const *  mus = dynamic_cast<New_repeated_music const*> (music_l_);
+  main_iter_p_ = get_iterator_p (mus->repeat_body_p_);
+  if (!main_iter_p_->ok())
+    {
+      leave_body ();
+      enter_alternative ();
+    }
+}
+
+void
+Folded_repeat_iterator::do_process_and_next (Moment m)
+{
+  if (!m)
+    {
+      bool success = report_to_l ()->try_music (music_l_);
       if (!success)
 	music_l_->warning ( _("No one to print a volta bracket"));
     }
-  if (repeat_iter_p_ && repeat_iter_p_->ok ())
-    repeat_iter_p_->process_and_next (m - here_mom_);
-  else
-    alternative_iter_p_->process_and_next (m - here_mom_);
-  Music_iterator::do_process_and_next (m);
-}
-
-Moment
-Repeated_music_iterator::next_moment () const
-{
   
-  if (repeat_iter_p_)
-    return repeat_iter_p_->next_moment () + here_mom_;
-  else if (alternative_iter_p_)
-    return alternative_iter_p_->next_moment () + here_mom_;
-
-  Repeated_music const*r = dynamic_cast<Repeated_music const*>(music_l_);
-  return r->alternative_p_->length_mom () + here_mom_;
-}
-
-/*
-  FIXME
- */
-bool
-Repeated_music_iterator::ok () const
-{
-  if (!repeat_iter_p_ && !alternative_iter_p_)
-    return false;
-
-  if ((repeat_iter_p_ && repeat_iter_p_->ok ())
-    || (alternative_iter_p_ && alternative_iter_p_->ok ()))
-    return true;
-
-  Repeated_music_iterator *urg = (Repeated_music_iterator*)this;
-  // urg, we're const
-  urg->start_next_element ();
-
-  return ok ();
-}
-
-
-void
-Repeated_music_iterator::start_next_element ()
-{
-  Repeated_music const*rep =dynamic_cast<Repeated_music const*> (music_l_);
-
-
- if (repeat_iter_p_)
+  New_repeated_music const * mus = dynamic_cast<New_repeated_music const*> (music_l_);
+  
+  if (main_iter_p_)
     {
-      assert (!repeat_iter_p_->ok ());
-      assert (!alternative_iter_p_);
-      delete repeat_iter_p_;
-      repeat_iter_p_ = 0;
-      alternative_iter_p_ = dynamic_cast<Music_list_iterator*>
-	(get_iterator_p ((Music*)rep->alternative_p_));  
-      here_mom_ += rep->repeat_p_->length_mom ();
+      main_iter_p_->process_and_next (m);
+      if (!main_iter_p_->ok ())
+	leave_body ();
     }
-  else if (alternative_iter_p_)
-    {
-      assert (!alternative_iter_p_->ok ());
-      assert (!repeat_iter_p_);
-      delete alternative_iter_p_;
-      alternative_iter_p_ = 0;
-      if (unfold_i_ < 0)
-	unfold_i_ = rep->unfold_b_ ? 
-	  rep->repeats_i_ - 1 : 0;
-      if (unfold_i_)
-        {
-	  unfold_i_--;
-	  repeat_iter_p_ = get_iterator_p (rep->repeat_p_);
-	  // urg, assume same length alternatives for now...
-//	  here_mom_ += rep->alternative_p_->music_p_list_p_->top ()->length_mom ();
-	  /*
-	    URG
-	    this is *wrong* but at least it doesn't dump core
-	    when unfolding, the alternative (sequential) music 
-	    shouldn't automatically move to the next alternative
 
-	    how to intercept this...
-	   */
-	  here_mom_ += rep->alternative_p_->length_mom ();
+  if (!main_iter_p_ && !alternative_iter_p_)
+    {
+      enter_alternative ();
+    }
+  
+  if (alternative_iter_p_)
+    {
+      alternative_iter_p_->process_and_next (m - main_length_mom_);
+      if (!alternative_iter_p_->ok ())
+	{
+	  delete alternative_iter_p_;
+	  alternative_iter_p_ =0;
 	}
     }
 }
 
+void
+Folded_repeat_iterator::leave_body ()
+{
+  New_repeated_music const *  mus = dynamic_cast<New_repeated_music const*> (music_l_);
+  delete main_iter_p_;
+  main_iter_p_ = 0;
+  main_length_mom_ +=  mus->repeat_body_p_->length_mom ();
+}
+
+void
+Folded_repeat_iterator::enter_alternative ()
+{
+  New_repeated_music const *  mus = dynamic_cast<New_repeated_music const*> (music_l_);  
+  Simultaneous_music_iterator * s = new Simultaneous_music_iterator;
+  s->separate_contexts_b_ = true;
+  s->init_translator (mus->alternatives_p_, report_to_l ());
+  
+  alternative_iter_p_ = s;
+  alternative_iter_p_->construct_children ();
+}
+
+void
+Folded_repeat_iterator::do_print () const
+{
+#ifndef NPRINT
+#endif
+}
--- lily/repeated-music.cc
+++ lily/repeated-music.cc
@@ -1,84 +1,130 @@
 /*   
-  repeated-music.cc --  implement Repeated_music
+  new-repeated-music.cc --  implement New_repeated_music
   
   source file of the GNU LilyPond music typesetter
   
-  (c) 1998--1999 Jan Nieuwenhuizen <janneke@gnu.org>
+  (c) 1999 Han-Wen Nienhuys <hanwen@cs.uu.nl>
   
  */
 
-#include "repeated-music.hh"
+#include "new-repeated-music.hh"
+#include "music-list.hh"
 #include "musical-pitch.hh"
+#include "debug.hh"
 
-Repeated_music::Repeated_music (Music* r, int n, Music_sequence* a)
+New_repeated_music::New_repeated_music(Music *beg, int times, Music_sequence * alts)
 {
-  repeats_i_ = n;
-  unfold_b_ = false;
-  repeat_p_ = r;
-  alternative_p_ = a;
+  repeat_body_p_ = beg;
+  fold_b_ = false;
+  repeats_i_ = times;
+  alternatives_p_ = alts;
+  semi_fold_b_ = true;
 }
 
-Repeated_music::~Repeated_music ()
-{
-  delete repeat_p_;
-  delete alternative_p_;
-}
-
-Repeated_music::Repeated_music (Repeated_music const& s)
+New_repeated_music::New_repeated_music (New_repeated_music const &s)
   : Music (s)
 {
   repeats_i_ = s.repeats_i_;
-  repeat_p_ = (s.repeat_p_) ? s.repeat_p_->clone () : 0;
-  // urg?
-  alternative_p_ = (s.alternative_p_) ? dynamic_cast <Music_sequence*> (s.alternative_p_->clone ()) : 0;
+  fold_b_ = s.fold_b_;
+  semi_fold_b_ = s.semi_fold_b_;
+  
+  repeat_body_p_ = s.repeat_body_p_ ? s.repeat_body_p_->clone () : 0;
+  alternatives_p_ = s.alternatives_p_
+    ? dynamic_cast<Music_sequence*> (s.alternatives_p_->clone ()):0;
+}
+
+New_repeated_music::~New_repeated_music ()
+{
+  delete repeat_body_p_;
+  delete alternatives_p_;
 }
 
 void
-Repeated_music::do_print () const
+New_repeated_music::do_print () const
 {
-  if (repeat_p_)
-    repeat_p_->print ();
-  if (alternative_p_)
-    alternative_p_->print ();
+#ifndef NPRINT
+  DOUT << "Fold = " << fold_b_ << " reps: " << repeats_i_;
+
+  if (repeat_body_p_)
+    repeat_body_p_->print();
+  
+  if (alternatives_p_)
+    alternatives_p_->print();
+#endif
+}
+
+Musical_pitch
+New_repeated_music::to_relative_octave (Musical_pitch p)
+{
+  if (repeat_body_p_)
+    p = repeat_body_p_->to_relative_octave (p);
+
+  if (alternatives_p_)
+    p = alternatives_p_->do_relative_octave (p, true);
+  return p;
+}
+
+
+void
+New_repeated_music::transpose (Musical_pitch p)
+{
+  if (repeat_body_p_)
+    repeat_body_p_->transpose (p);
+
+  if (alternatives_p_)
+    alternatives_p_->transpose (p);  
 }
 
 void
-Repeated_music::transpose (Musical_pitch p)
+New_repeated_music::compress (Moment p)
 {
-  if (repeat_p_)
-    repeat_p_->transpose (p);
-  if (alternative_p_)
-    alternative_p_->transpose (p);
+  if (repeat_body_p_)
+    repeat_body_p_->compress (p);
+
+  if (alternatives_p_)
+    alternatives_p_->compress (p);  
 }
 
 Moment
-Repeated_music::length_mom () const
+New_repeated_music::alternatives_length_mom () const
 {
-  Moment m;
-  if (repeat_p_)
-    m += repeat_p_->length_mom ();
-  if (alternative_p_)
-    m += alternative_p_->length_mom ();
+  if (!alternatives_p_ )
+    return 0;
+  
+  if  (fold_b_)
+    alternatives_p_->maximum_length ();
+
+  Moment m =0;
+  int done =0;
+  Cons<Music> *p = alternatives_p_->music_p_list_p_->head_;
+  while (p && done < repeats_i_)
+    {
+      m = m + p->car_->length_mom ();
+      done ++;
+      if (repeats_i_ - done < alternatives_p_->length_i ())
+	p = p->next_;
+    }
   return m;
 }
 
-
-Musical_pitch
-Repeated_music::to_relative_octave (Musical_pitch p)
+Moment
+New_repeated_music::length_mom () const
 {
-  p = repeat_p_->to_relative_octave (p);
+  Moment m =0;
+  if (fold_b_)
+    {
+      if (repeat_body_p_)
+	m += repeat_body_p_->length_mom ();
+    }
+  else
+    {
+      Moment beg = (repeat_body_p_) ? repeat_body_p_->length_mom () : Rational(0);
+      if (!semi_fold_b_)
+	beg *=  Rational (repeats_i_);
+      m += beg;
+    }
 
-  p = alternative_p_->do_relative_octave (p, false); 
-  return p;
-  
-  /* ugh.  Should 
-     \relative c'' \repeat 2  { c4 } { < ... > }
-
-     and 
-     
-     \relative c'' \repeat 2  { c4 }
-     { { ...} }
-
-     behave differently?
-   */
+  m += alternatives_length_mom ();
+  return m;
 }
+
