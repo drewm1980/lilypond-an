7018b33a274ba6764a869b10d296343731e9ec9c
hanwen <hanwen>
2002-04-20 23:02:55 +0000

    ''

---
+++ lily/include/script.hh
@@ -0,0 +1,32 @@
+/*   
+  script.hh -- declare Script
+  
+  source file of the GNU LilyPond music typesetter
+  
+  (c) 1999--2002 Han-Wen Nienhuys <hanwen@cs.uu.nl>
+  
+ */
+
+#ifndef SCRIPT_HH
+#define SCRIPT_HH
+
+#include "lily-guile.hh"
+#include "lily-proto.hh"
+
+/**
+   Articulation marks (and the like) that are attached to notes/stems.
+   Needs support from Staff_side for proper operation.  Staff_side
+   handles the positioning.
+*/
+class Script
+{
+public:
+  static Molecule get_molecule (Grob*,Direction d);
+  
+  static bool  has_interface (Grob*);
+  DECLARE_SCHEME_CALLBACK (brew_molecule, (SCM ));
+  DECLARE_SCHEME_CALLBACK (before_line_breaking, (SCM ));
+};
+
+#endif /* Stem_SCRIPT_HH */
+
