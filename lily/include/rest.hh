df38e038c22ca157165cf68a8211b8442d578a5c
janneke <janneke>
2003-04-24 14:10:12 +0000

    * Another grand 2003 update.
    	* Remove mentioning of `mudela'; There's no such thing.

---
+++ lily/include/rest.hh
@@ -0,0 +1,25 @@
+/*
+  rest.hh -- declare Rest
+
+  source file of the GNU LilyPond music typesetter
+
+  (c)  1997--2003 Han-Wen Nienhuys <hanwen@cs.uu.nl>
+*/
+
+
+#ifndef REST_HH
+#define REST_HH
+
+#include "lily-guile.hh"
+
+class  Rest
+{
+public:
+  DECLARE_SCHEME_CALLBACK (after_line_breaking, (SCM ));
+  static bool has_interface (Grob*);
+  static String glyph_name (Grob*, int, String); 
+  static SCM brew_internal_molecule (SCM);
+  DECLARE_SCHEME_CALLBACK (extent_callback, (SCM,SCM));
+  DECLARE_SCHEME_CALLBACK (brew_molecule, (SCM ));
+};
+#endif // REST_HH
---
+++ lily/include/text-spanner.hh
@@ -0,0 +1,24 @@
+/*
+  text-spanner.hh -- declare Text_spanner
+
+  source file of the GNU LilyPond music typesetter
+
+  (c)  2000--2003 Jan Nieuwenhuizen <janneke@gnu.org>
+*/
+
+
+#ifndef TEXT_SPANNER_HH
+#define TEXT_SPANNER_HH
+
+#include "lily-guile.hh"
+
+class Text_spanner
+{
+public:
+  DECLARE_SCHEME_CALLBACK (brew_molecule, (SCM));
+  static bool has_interface (Grob*);
+private:
+  static void setup_pedal_bracket(Spanner *s);
+};
+
+#endif /* TEXT_SPANNER_HH */
